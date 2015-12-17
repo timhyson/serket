8ac9d557c199a7f345956dc8db18888d936fb026
chweeks <chrisharryweeks@gmail.com>
2015-12-17 11:33:06 +0000

    setup controller and api call

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var serket = angular.module('serket', ['ngRoute'])
+
+serket.config(['$routeProvider', function($routeProvider) {
+
+  $routeProvider.otherwise({redirectTo: '/home'});
+
+  $routeProvider.when('/home', {
+    templateUrl: 'home/home.html',
+    controller: 'homeCtrl'
+  });
 }]);
---
+++ app/home/homeController.js
@@ -0,0 +1,18 @@
+'use strict';
+
+serket.controller('homeCtrl', ['$http', function($http) {
+
+  var self = this;
+
+  self.joinParty = function() {
+    $http.get('http://serket.uk/badges/badgelist', 'GET').then(
+      function success(response) {
+        console.log(response.data.length);
+      },
+      function error(response) {
+        alert("Information not available")
+      });
+  }
+
+  self.joinParty();
+}]);
