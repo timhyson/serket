6c8aaf47ea6e52f298f43e2c7c3a7a57afb84a3f
chweeks <chrisharryweeks@gmail.com>
2015-12-17 12:20:00 +0000

    added chartjs

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -4,7 +4,7 @@ serket.controller('homeCtrl', ['$http', function($http) {
 
   var self = this;
 
-  self.joinParty = function() {
+  self.getData = function() {
     $http.get('http://serket.uk/badges/badgelist', 'GET').then(
       function success(response) {
         console.log(response.data.length);
